b6e6a2ffe2f0c9f1b3b1f9dac62887f3cf2ebfed
movmovmovmov <kej708@gmail.com>
2023-06-20 08:07:04 +0900

    [kej] 도메인 추가

---
+++ src/main/java/project/rlozi/domain/task/task.java
@@ -0,0 +1,23 @@
+package project.rlozi.domain.task;
+
+public class task {
+
+    private Long idx;
+
+    private String start_date;
+
+    private String end_date;
+
+    private String status;
+
+    private String client_ref;
+
+    private String user_ref;
+
+    private String title;
+
+    private String author;
+
+
+
+}
